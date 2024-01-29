d5c75d7940fc50c0eaad20558e8e0cf58f0d378e
uday11@gmail.com <uday11@gmail.com>
2024-01-29 07:30:12 +0000

    mfile

--- script1.sh
+++ script1.sh
@@ -4,6 +4,8 @@ echo " enter sal"
 read sal
 if [ $sal -lt 20000 ];then
         echo " no tax"
+elif [ $sal -eq 2000 ];then
+	echo " 2% tax "
 elif [ $sal -gt 20000 -a $sal -le 40000 ];then
         echo " 5% tax"
 elif [ $sal -gt 40000 ];then
--- script3.sh
+++ script3.sh
@@ -7,12 +7,14 @@ echo " select one"
 echo " 1.addition"
 echo " 2.subtraction"
 echo " 3. multiplication"
+echo " 4. divsion"
 read one
 calculate (){
         case $1 in
                 1) echo " output : $(( a + b ))";;
                 2) echo " output : $(( a - b ))";;
                 3) echo " output : $(( a * b ))";;
+		4) echo " output : $(( a / b ))";;
                 *) echo " invalid";;
         esac
 }
